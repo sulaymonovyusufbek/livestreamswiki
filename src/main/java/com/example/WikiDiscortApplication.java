6b7c591b215faa362d6136fe7d497bc1028e97d3
nitro <sulaymonov.yusufbek@gmail.com>
2025-02-09 14:14:15 +0100

    abs

--- src/main/java/com/example/WikiDiscortApplication.java
+++ src/main/java/com/example/WikiDiscortApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WikiDiscortApplication {
+public class
+WikiDiscortApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WikiDiscortApplication.class, args);
